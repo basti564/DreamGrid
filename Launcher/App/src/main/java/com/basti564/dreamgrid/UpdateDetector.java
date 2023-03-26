299b8518308b6e5f224a036fbf99d75ffe22d957
Bastian <34898868+basti564@users.noreply.github.com>
2023-03-26 21:44:49 +0200

    use lambda expressions in UpdateDetector

--- Launcher/App/src/main/java/com/basti564/dreamgrid/UpdateDetector.java
+++ Launcher/App/src/main/java/com/basti564/dreamgrid/UpdateDetector.java
@@ -2,7 +2,6 @@ package com.basti564.dreamgrid;
 
 import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
@@ -12,8 +11,6 @@ import android.view.WindowManager;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -39,53 +36,37 @@ public class UpdateDetector {
 
             StringRequest updateRequest = new StringRequest(
                     Request.Method.GET, "https://api.github.com/repos/basti564/Oculess/releases/latest",
-                    new Response.Listener<String>() {
-                        @Override
-                        public void onResponse(String response) {
-                            try {
-                                JSONObject latestReleaseJson = (JSONObject) new JSONTokener(response).nextValue();
-                                if (!("v" + packageInfo.versionName).equals(latestReleaseJson.getString("tag_name"))) {
-                                    Log.v("DreamGrid", "New version available!!!!");
+                    response -> {
+                        try {
+                            JSONObject latestReleaseJson = (JSONObject) new JSONTokener(response).nextValue();
+                            if (!("v" + packageInfo.versionName).equals(latestReleaseJson.getString("tag_name"))) {
+                                Log.v("DreamGrid", "New version available!!!!");
 
-                                    AlertDialog.Builder updateDialogBuilder = new AlertDialog.Builder(appContext);
-                                    updateDialogBuilder.setTitle("An update is available!");
-                                    updateDialogBuilder.setMessage("We recommend you to update to the latest version of DreamGrid (" +
-                                            latestReleaseJson.getString("tag_name") + ")");
-                                    updateDialogBuilder.setPositiveButton("View", new DialogInterface.OnClickListener() {
-                                        @Override
-                                        public void onClick(DialogInterface dialog, int which) {
-                                            Intent browserIntent = null;
-                                            try {
-                                                browserIntent = new Intent(Intent.ACTION_VIEW,
-                                                        Uri.parse(latestReleaseJson.getString("html_url")));
-                                            } catch (JSONException e) {
-                                                e.printStackTrace();
-                                            }
-                                            appContext.startActivity(browserIntent);
-                                        }
-                                    });
-                                    updateDialogBuilder.setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
-                                        @Override
-                                        public void onClick(DialogInterface dialog, int which) {
-                                            dialog.dismiss();
-                                        }
-                                    });
-                                    AlertDialog updateAlertDialog = updateDialogBuilder.create();
-                                    updateAlertDialog.show();
-                                    updateAlertDialog.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
-                                } else {
-                                    Log.i("DreamGrid", "DreamGrid is up to date :)");
-                                }
-                            } catch (JSONException e) {
-                                Log.e("DreamGrid", "Received invalid JSON", e);
+                                AlertDialog.Builder updateDialogBuilder = new AlertDialog.Builder(appContext);
+                                updateDialogBuilder.setTitle("An update is available!");
+                                updateDialogBuilder.setMessage("We recommend you to update to the latest version of DreamGrid (" +
+                                        latestReleaseJson.getString("tag_name") + ")");
+                                updateDialogBuilder.setPositiveButton("View", (dialog, which) -> {
+                                    Intent browserIntent = null;
+                                    try {
+                                        browserIntent = new Intent(Intent.ACTION_VIEW,
+                                                Uri.parse(latestReleaseJson.getString("html_url")));
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                    appContext.startActivity(browserIntent);
+                                });
+                                updateDialogBuilder.setNegativeButton("Dismiss", (dialog, which) -> dialog.dismiss());
+                                AlertDialog updateAlertDialog = updateDialogBuilder.create();
+                                updateAlertDialog.show();
+                                updateAlertDialog.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+                            } else {
+                                Log.i("DreamGrid", "DreamGrid is up to date :)");
                             }
+                        } catch (JSONException e) {
+                            Log.e("DreamGrid", "Received invalid JSON", e);
                         }
-                    }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    Log.w("DreamGrid", "Couldn't get update info");
-                }
-            });
+                    }, error -> Log.w("DreamGrid", "Couldn't get update info"));
 
             requestQueue.add(updateRequest);
 
